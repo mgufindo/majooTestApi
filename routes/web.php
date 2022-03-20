0cb4dcb007b5a27e3701f13fb90e8fbf9546b7cf
muhammadgufindoalenra <mgufindo@gmail.com>
2022-03-20 16:44:22 +0700

    product

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,7 @@ $router->post("/login", "LoginController@login");
 
 $router->group(['prefix' => 'product', 'middleware' => [\App\Http\Middleware\Cors::class]], function () use ($router) {
     $router->get('/', 'ProductController@allProduct');
+    $router->get("/list", "ProductController@listProduct");
     $router->post("/", "ProductController@create");
 });
 
