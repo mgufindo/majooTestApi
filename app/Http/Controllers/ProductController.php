34961c1f8dcec9ed749e3a8d0641e1a6b60034fa
muhammadgufindoalenra <mgufindo@gmail.com>
2022-03-21 02:54:09 +0700

    kategori

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Http\Controllers;
 
 use App\Models\Product;
@@ -10,13 +11,13 @@ class ProductController extends Controller
 {
     public function allProduct()
     {
-        $response = Product::where("status",1)->get();
+        $response = Product::where("status", 1)->get();
         return Result::SUCCESS($response);
     }
 
     public function listProduct()
     {
-        return DataTables::of(Product::where('status',1)->get())->toJson();
+        return DataTables::of(Product::where('status', 1)->get())->toJson();
     }
 
     public function create(Request $request)
@@ -37,5 +38,25 @@ class ProductController extends Controller
         }
 
         $cek->save();
+    }
+
+    public function dataProductEdit($id, Request $request)
+    {
+        $result = Product::where("id", $id)->first();
+        return Result::SUCCESS($result);
+    }
+
+    public function update(Request $request)
+    {
+         $result = Product::where("id", $request->get("id"))->first();
+
+        $result->nama_produk = empty($request->get("nama_produk")) ? $result->nama_produk : $request->get("nama_produk");
+        $result->deskripsi_produk = empty($request->get("deskripsi_produk")) ? $result->deskripsi_produk : $request->get("deskripsi_produk");
+        $result->harga_produk = empty($request->get("harga_produk"))? $result->harga_produk : $request->get("harga_produk");
+        $result->kategori_id = empty($request->get("kategori_id"))? $result->kategory_id : $request->get("kategori_id");
+        $result->image = empty($request->get("image")) ? $result->image : $request->get("image");
+        $result->status = 1;
+        $result->save();
+        return Result::SUCCESS('success');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,8 @@ $router->post("/login", "LoginController@login");
 $router->group(['prefix' => 'product', 'middleware' => [\App\Http\Middleware\Cors::class]], function () use ($router) {
     $router->get('/', 'ProductController@allProduct');
     $router->get("/list", "ProductController@listProduct");
+    $router->get("/data-edit/{id}", "ProductController@dataProductEdit");
+    $router->post('update', 'ProductController@update');
     $router->post("/", "ProductController@create");
     $router->post("/delete", "ProductController@delete");
 });
